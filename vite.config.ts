92171447a4942d9075e4395e6df534e2e77f1e30
peak <zpeak.dev@outlook.com>
2026-03-27 15:30:15 +0800

    feat: 集成 Tailwind CS 并配置相关构建规则

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from "node:path";
 import { defineConfig } from "vite";
 import { crx } from "@crxjs/vite-plugin";
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import manifest from "./manifest.config.ts";
 
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     crx({ manifest }),
   ],
   server: {
